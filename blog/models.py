a6fa08fa48c1c38efe92407050461efbd7add5a7
Asif Haider <1805112@ugrad.cse.buet.ac.bd>
2020-09-13 19:36:32 +0600

    added database, migrations and user app for frontend input

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField() #unrestricted length
+    date_posted = models.DateTimeField(default=timezone.now) # while creating new instance update date time
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #user deleted, auto deletes posts
+
+    # dunder means double underscore, magic methods or bla bla
+    # declared to return title in shell sql manipulation
+
+    def __str__(self):
+        return self.title
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
  
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Asif',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content, Extreme Lorum Epsum',
-        'date': 'September 13, 2020'
-    },
-    {
-        'author': 'Elhan',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content, Extreme Lorum Epsum',
-        'date': 'September 14, 2020'
-    }
+#### DUMMY POST DATA, REMOVED LATER 
+
+# posts = [
+#     {
+#         'author': 'Asif',
+#         'title': 'Blog Post 1',
+#         'content': 'First Post Content, Extreme Lorum Epsum',
+#         'date': 'September 13, 2020'
+#     },
+#     {
+#         'author': 'Elhan',
+#         'title': 'Blog Post 2',
+#         'content': 'Second Post Content, Extreme Lorum Epsum',
+#         'date': 'September 14, 2020'
+#     }
     
-]
+# ]
 
 
 def home (request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
