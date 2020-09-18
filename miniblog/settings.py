646e50f471f1616fce5a6bf0242ff2f43c94ee62
Asif Haider <1805112@ugrad.cse.buet.ac.bd>
2020-09-18 16:30:40 +0600

    password reset

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -138,3 +138,12 @@ LOGIN_REDIRECT_URL = 'blog-home'
 # login path variable fixing
 
 LOGIN_URL = 'login'
+
+# password reset 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
